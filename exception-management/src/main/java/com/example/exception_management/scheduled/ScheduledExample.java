0b1c424531dc721133bcccbe7469adb97fa696a9
Oktay Erdogan <oktayerdogan2000@hotmail.com>
2025-09-07 21:35:51 +0300

    Scheduled Kullanımı

---
+++ exception-management/src/main/java/com/example/exception_management/scheduled/ScheduledExample.java
@@ -0,0 +1,15 @@
+package com.example.exception_management.scheduled;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledExample {
+
+    @Scheduled(cron = "*/5 * * * * *")
+    public void write1to10(){
+        for (int i=1 ; i<=10; i++){
+            System.out.print(i+" ");
+        }
+    }
+}
