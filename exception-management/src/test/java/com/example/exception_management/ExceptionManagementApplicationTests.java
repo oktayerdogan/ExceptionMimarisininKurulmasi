d5b8dd35b720b6018c3e693edf438ad2911c201f
Oktay Erdogan <oktayerdogan2000@hotmail.com>
2025-09-08 16:58:06 +0300

    JUnit ile Test Yazmak

--- exception-management/src/test/java/com/example/exception_management/ExceptionManagementApplicationTests.java
+++ exception-management/src/test/java/com/example/exception_management/ExceptionManagementApplicationTests.java
@@ -1,13 +1,38 @@
 package com.example.exception_management;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.example.exception_management.dto.DtoEmployee;
+import com.example.exception_management.service.IEmployeeService;
+
+@SpringBootTest(classes = {ExceptionManagementApplication.class})
 class ExceptionManagementApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	private IEmployeeService employeeService;
+
+	@BeforeEach
+	public void beforeEach(){
+		System.out.println("Before each çalıştı");
 	}
 
+	@Test
+	public void testFindEmployeeById(){
+		DtoEmployee employee = employeeService.findEmployeeById(2L);
+		if(employee!=null){
+			System.out.println("isim :" + employee.getName());
+		}
+		assertEquals(2,employee.getId());
+	}
+
+	@AfterEach
+	public void afterEach(){
+		System.out.println("afterEach");
+	}
 }
